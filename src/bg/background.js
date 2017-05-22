b55240183a84f6b5a0995d42f35c7356e69f93e5
ralphchen <diverboxer@gmail.com>
2017-05-22 17:55:49 +0800

    Remove new tab and use browser action

--- src/bg/background.js
+++ src/bg/background.js
@@ -8,3 +8,7 @@ chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     console.log('background triggered');
     chrome.tabs.executeScript(null,{file:"src/inject/inject.js"});
 });
+
+chrome.browserAction.onClicked.addListener(function () {
+    chrome.tabs.create({ url: chrome.runtime.getURL("src/override/override.html") });
+});
